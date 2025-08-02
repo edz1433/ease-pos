304a7c3b771c204bbc5de9558cbb0808e2e95b40
edz1433 <edzavril1@gmail.com>
2025-08-02 16:47:33 +0800

    push

---
+++ storage/framework/views/57e5d6879ab1d557607ab7dc4b7c1f013f4b65ba.php
@@ -0,0 +1,112 @@
+<script>
+$(document).ready(function () {
+    let retailPriceManuallyEdited = false;
+
+    function calculateRetailFields() {
+        const packaging = parseFloat($('#packaging').val()) || 1;
+        const wCapital = parseFloat($('#whole_capital').val()) || 0;
+
+        if (packaging > 1 && wCapital > 0) {
+            const rCapital = wCapital / packaging;
+            $('#retail_capital').val(rCapital.toFixed(2)).prop('readonly', true);
+
+            if (!retailPriceManuallyEdited) {
+                const rPrice = rCapital * 1.10;
+                $('#retail_price').val(rPrice.toFixed(2));
+            }
+
+            $('#retail_price').prop('readonly', false);
+        } else if (packaging === 1) {
+            $('#retail_capital, #retail_price').prop('readonly', false);
+        }
+    }
+
+    function handleMadeToOrder() {
+        const isMTO = $('#product_type').val() === 'made-to-order';
+
+        if (isMTO) {
+            $('#whole_price, #whole_capital, #packaging, #retail_capital, #retail_price')
+                .val('0.00')
+                .prop('readonly', true);
+            $('#retail_unit, #wholesale_unit').val('').prop('readonly', true);
+        } else {
+            $('#packaging').prop('readonly', false);
+            $('#retail_unit, #wholesale_unit').prop('readonly', false);
+            handlePackaging();
+        }
+    }
+
+    function handlePackaging() {
+        const packaging = parseFloat($('#packaging').val()) || 1;
+
+        if (packaging === 1) {
+            $('#whole_price, #whole_capital').val('0.00').prop('readonly', true);
+            $('#wholesale_unit').val('').prop('readonly', true);
+
+            $('#retail_capital, #retail_price').prop('readonly', false);
+        } else {
+            $('#whole_price, #whole_capital').prop('readonly', false);
+            $('#wholesale_unit').prop('readonly', false);
+
+            $('#retail_capital').prop('readonly', true);
+        }
+    }
+
+    $('#retail_price').on('input', function () {
+        retailPriceManuallyEdited = true;
+    });
+
+    $('#packaging, #whole_capital').on('input', function () {
+        retailPriceManuallyEdited = false;
+        handlePackaging();
+        calculateRetailFields();
+    });
+
+    $('#product_type').on('change', function () {
+        retailPriceManuallyEdited = false;
+        handleMadeToOrder();
+        calculateRetailFields();
+    });
+
+    // Initial run
+    handleMadeToOrder();
+    calculateRetailFields();
+});
+</script>
+<script>
+$(document).ready(function () {
+    $('#barcode').on('input', function () {
+        const barcode = $(this).val();
+
+        if (barcode.length > 0) {
+            $.ajax({
+                url: '<?php echo e(route("getProductPresets")); ?>',
+                method: 'GET',
+                data: { barcode: barcode },
+                success: function (response) {
+                    if (response.success && response.preset) {
+                        const preset = response.preset;
+                        $('#product_name').val(preset.product_name);
+                    }
+                }
+            });
+        }
+    });
+});
+</script>
+<script>
+    function generateBarcode() {
+        fetch('<?php echo e(route('getNextBarcode')); ?>')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('barcode').value = data.next_barcode;
+            })
+            .catch(error => {
+                alert('Error generating barcode');
+                console.error(error);
+            });
+    }
+</script>
+
+
+<?php /**PATH F:\xampp\htdocs\ease-pos\resources\views/script/productScript.blade.php ENDPATH**/ ?>
--- storage/framework/views/5c10bb06d4c4311a6468bf5b3e475b3883b47b1a.php
+++ storage/framework/views/5c10bb06d4c4311a6468bf5b3e475b3883b47b1a.php
@@ -1,6 +1,6 @@
-@extends('layouts.master')
 
-@section('body')
+
+<?php $__env->startSection('body'); ?>
 <style>
     .bg-form{
         background-color:  #e9ecef;
@@ -51,77 +51,80 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">ADD PRODUCT</div>
                                 <div class="panel-body bg-form">
-                                    <form class="p-2" id="product_form_data" method="POST" action="{{ isset($productedit) ? route('productUpdate', $productedit->id) : route('productCreate') }}" enctype="multipart/form-data">
-                                        @csrf
-                                        @if(isset($productedit))
-                                            @method('PUT')
-                                            <input type="hidden" name="id" value="{{ $productedit->id }}">
-                                        @endif
+                                    <form class="p-2" id="product_form_data" method="POST" action="<?php echo e(isset($productedit) ? route('productUpdate', $productedit->id) : route('productCreate')); ?>" enctype="multipart/form-data">
+                                        <?php echo csrf_field(); ?>
+                                        <?php if(isset($productedit)): ?>
+                                            <?php echo method_field('PUT'); ?>
+                                            <input type="hidden" name="id" value="<?php echo e($productedit->id); ?>">
+                                        <?php endif; ?>
 
                                         <div class="form-group">
                                             <label for="barcode">Barcode <i class="fas fa-sync" onclick="generateBarcode()"></i></label>
-                                            <input type="text" name="barcode" value="{{ $productedit->barcode ?? '' }}" class="form-control form-control-sm" id="barcode" required oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                                            <input type="text" name="barcode" value="<?php echo e($productedit->barcode ?? ''); ?>" class="form-control form-control-sm" id="barcode" required oninput="this.value = this.value.replace(/[^0-9]/g, '')">
                                             
                                             <label for="product_name">Product Name</label>
-                                            <input type="text" name="product_name" value="{{ $productedit->product_name ?? '' }}" class="form-control form-control-sm" id="product_name" required>
+                                            <input type="text" name="product_name" value="<?php echo e($productedit->product_name ?? ''); ?>" class="form-control form-control-sm" id="product_name" required>
 
                                             <label for="product_name">Product Type</label>
                                             <select name="product_type" id="product_type" class="form-control form-control-sm" required>
                                                 <option value="">-- Select Type --</option>
-                                                <option value="1" {{ ($productedit->product_type ?? '') == '1' ? 'selected' : '' }}>Standard</option>
-                                                <option value="2" {{ ($productedit->product_type ?? '') == '2' ? 'selected' : '' }}>Made to Order</option>
+                                                <option value="1" <?php echo e(($productedit->product_type ?? '') == '1' ? 'selected' : ''); ?>>Standard</option>
+                                                <option value="2" <?php echo e(($productedit->product_type ?? '') == '2' ? 'selected' : ''); ?>>Made to Order</option>
                                             </select>
 
                                             <label for="category">Category</label>
                                             <select name="category" id="category" class="form-control form-control-sm" required>
                                                 <option value="">-- Select Category --</option>
-                                                @foreach ($categories as $category)
-                                                    <option value="{{ $category->id }}" 
-                                                        {{ ($productedit->category ?? '') == $category->id ? 'selected' : '' }}>
-                                                        {{ $category->name }}
+                                                <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                    <option value="<?php echo e($category->id); ?>" 
+                                                        <?php echo e(($productedit->category ?? '') == $category->id ? 'selected' : ''); ?>>
+                                                        <?php echo e($category->name); ?>
+
                                                     </option>
-                                                @endforeach
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                             </select>
 
 
                                             <label for="packaging">Packaging</label>
-                                            <input type="number" name="packaging" value="{{ $productedit->packaging ?? '' }}" class="form-control form-control-sm" id="packaging" required>
+                                            <input type="number" name="packaging" value="<?php echo e($productedit->packaging ?? ''); ?>" class="form-control form-control-sm" id="packaging" required>
 
                                              <label for="w_capital">Wholesale Capital</label>
-                                            <input type="number" step="0.01" name="w_capital" value="{{ $productedit->w_capital ?? '' }}" class="form-control form-control-sm" id="whole_capital" required>
+                                            <input type="number" step="0.01" name="w_capital" value="<?php echo e($productedit->w_capital ?? ''); ?>" class="form-control form-control-sm" id="whole_capital" required>
 
                                             <label for="w_price">Wholesale Price</label>
-                                            <input type="number" step="0.01" name="w_price" value="{{ $productedit->w_price ?? '' }}" class="form-control form-control-sm" id="whole_price" required>
+                                            <input type="number" step="0.01" name="w_price" value="<?php echo e($productedit->w_price ?? ''); ?>" class="form-control form-control-sm" id="whole_price" required>
 
                                             <label for="w_unit">Wholesale Unit</label>
                                             <select name="w_unit" class="form-control form-control-sm" id="wholesale_unit">
                                                 <option value="">-- Select Unit --</option>
-                                                @foreach ($units as $unit)
-                                                    <option value="{{ $unit->id }}" {{ ($productedit->w_unit ?? '') == $unit->id ? 'selected' : '' }}>
-                                                        {{ $unit->name }}
+                                                <?php $__currentLoopData = $units; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $unit): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                    <option value="<?php echo e($unit->id); ?>" <?php echo e(($productedit->w_unit ?? '') == $unit->id ? 'selected' : ''); ?>>
+                                                        <?php echo e($unit->name); ?>
+
                                                     </option>
-                                                @endforeach
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                             </select>
 
                                             <label for="r_capital">Retail Capital</label>
-                                            <input type="number" step="0.01" name="r_capital" value="{{ $productedit->r_capital ?? '' }}" class="form-control form-control-sm" id="retail_capital" required>
+                                            <input type="number" step="0.01" name="r_capital" value="<?php echo e($productedit->r_capital ?? ''); ?>" class="form-control form-control-sm" id="retail_capital" required>
 
                                             <label for="r_price">Retail Price</label>
-                                            <input type="number" step="0.01" name="r_price" value="{{ $productedit->r_price ?? '' }}" class="form-control form-control-sm" id="retail_price" required>
+                                            <input type="number" step="0.01" name="r_price" value="<?php echo e($productedit->r_price ?? ''); ?>" class="form-control form-control-sm" id="retail_price" required>
 
                                             <label for="r_unit">Retail Unit</label>
                                             <select name="r_unit" class="form-control form-control-sm" id="wholesale_unit" required>
                                                 <option value="">-- Select Unit --</option>
-                                                @foreach ($units as $unit)
-                                                    <option value="{{ $unit->id }}" {{ ($productedit->r_unit ?? '') == $unit->id ? 'selected' : '' }}>
-                                                        {{ $unit->name }}
+                                                <?php $__currentLoopData = $units; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $unit): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                    <option value="<?php echo e($unit->id); ?>" <?php echo e(($productedit->r_unit ?? '') == $unit->id ? 'selected' : ''); ?>>
+                                                        <?php echo e($unit->name); ?>
+
                                                     </option>
-                                                @endforeach
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                             </select>
                                         </div>
 
                                         <label for="image">Image</label>
-                                        <input type="file" name="image" class="form-control form-control-sm" id="image" {{ isset($productedit) ? '' : 'required' }}>
+                                        <input type="file" name="image" class="form-control form-control-sm" id="image" <?php echo e(isset($productedit) ? '' : 'required'); ?>>
                                         
 
                                         <button type="submit" class="btn btn-success mt-2">
@@ -158,28 +161,29 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach($products as $product)
+                                            <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                                 <tr>
                                                     <td>
-                                                        <img src="{{ asset('storage/' . $product->image) }}" class="product-image" alt="Product Image">
+                                                        <img src="<?php echo e(asset('storage/' . $product->image)); ?>" class="product-image" alt="Product Image">
                                                     </td>
-                                                    <td class="text-main-10">{{ $product->barcode }}</td>
-                                                    <td class="text-main-8">{{ $product->product_name }}</td>
-                                                    <td class="text-main-1">{{ $product->category }}</td>
+                                                    <td class="text-main-10"><?php echo e($product->barcode); ?></td>
+                                                    <td class="text-main-8"><?php echo e($product->product_name); ?></td>
+                                                    <td class="text-main-1"><?php echo e($product->category); ?></td>
                                                     <td class="text-main-3">
-                                                        {{ $product->product_type == '1' ? 'Retail & Wholesale' : 'Retail Only' }}
+                                                        <?php echo e($product->product_type == '1' ? 'Retail & Wholesale' : 'Retail Only'); ?>
+
                                                     </td>
-                                                    <td>{{ $product->packaging }}</td>
-                                                    <td>₱{{ number_format($product->w_capital, 2) }}</td>
-                                                    <td>₱{{ number_format($product->w_price, 2) }}</td>
-                                                    <td>{{ $product->w_unit ?? '-' }}</td>
-                                                    <td>₱{{ number_format($product->r_capital, 2) }}</td>
-                                                    <td class="text-main-4">₱{{ number_format($product->r_price, 2) }}</td>
-                                                    <td>{{ $product->r_unit ?? '-' }}</td>
-                                                    <td class="text-main-7">{{ number_format($product->rqty) }}</td>
-                                                    <td class="text-main-6">{{ number_format($product->wqty) }}</td>
+                                                    <td><?php echo e($product->packaging); ?></td>
+                                                    <td>₱<?php echo e(number_format($product->w_capital, 2)); ?></td>
+                                                    <td>₱<?php echo e(number_format($product->w_price, 2)); ?></td>
+                                                    <td><?php echo e($product->w_unit ?? '-'); ?></td>
+                                                    <td>₱<?php echo e(number_format($product->r_capital, 2)); ?></td>
+                                                    <td class="text-main-4">₱<?php echo e(number_format($product->r_price, 2)); ?></td>
+                                                    <td><?php echo e($product->r_unit ?? '-'); ?></td>
+                                                    <td class="text-main-7"><?php echo e(number_format($product->rqty)); ?></td>
+                                                    <td class="text-main-6"><?php echo e(number_format($product->wqty)); ?></td>
                                                     <td class="text-center">
-                                                        <a href="{{ route('productEdit', $product->id) }}" class="text-main-10" title="Edit">
+                                                        <a href="<?php echo e(route('productEdit', $product->id)); ?>" class="text-main-10" title="Edit">
                                                             <i class="fas fa-edit fa-lg"></i>
                                                         </a>
                                                         <a href="#" class="text-main" title="Delete" onclick="return confirm('Are you sure you want to delete this product?')">
@@ -187,7 +191,7 @@
                                                         </a>
                                                     </td>
                                                 </tr>
-                                            @endforeach
+                                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         </tbody>
                                     </table>
                                 </div>
@@ -199,8 +203,8 @@
         </div>
     </div>
 </div>
-<form id="post-form" action="{{ route('userEdit') }}" method="POST" style="display: none;">
-    @csrf
+<form id="post-form" action="<?php echo e(route('userEdit')); ?>" method="POST" style="display: none;">
+    <?php echo csrf_field(); ?>
     <input type="hidden" name="id" id="user-id">
 </form>
 <script>
@@ -210,4 +214,5 @@
         document.getElementById('post-form').submit();
     }
 </script>
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\xampp\htdocs\ease-pos\resources\views/admin/products/index.blade.php ENDPATH**/ ?>
